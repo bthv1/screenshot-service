bb5faab99646ac1a7157da166dde425e286ab625
Baptiste Thevelein <baptiste@thevelein.fr>
2025-05-22 14:31:03 +0200

    Updated Chromium

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const puppeteer = require('puppeteer-core');
-const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer');
 const path = require('path');
 
 (async () => {
-  const executablePath = await chromium.executablePath;
-  
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--headless'
+    ],
+    executablePath: process.env.CHROME_BIN || null
   });
 
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 800 });
 
-  const url = process.env.TARGET_URL || "https://vert.eco";
+  const url = process.env.TARGET_URL || "https://example.com";
   await page.goto(url, { waitUntil: 'networkidle2' });
 
   await page.screenshot({
