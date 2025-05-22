434eadc72a8d5ab1c5a71e7f36309133be478f83
Baptiste Thevelein <baptiste@thevelein.fr>
2025-05-22 12:24:33 +0200

    Add index.js and server.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 800 });
+
+  const url = process.env.TARGET_URL || "https://example.com";
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  await page.screenshot({
+    path: path.join(__dirname, 'public', 'latest.png'),
+    clip: { x: 0, y: 0, width: 1200, height: 400 }
+  });
+
+  await browser.close();
+})();
